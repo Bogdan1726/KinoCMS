65927df2274db5906796e682959ab04a5f50cb0b
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2022-01-20 02:02:09 +0200

    add base templates cms and pages cms, add static admin lte

--- cinema/cms/urls.py
+++ cinema/cms/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='cms'),
+    path('', statistics, name='cms'),
+    path('list_movie/', list_movie, name='list_movie'),
+    path('page_movie/', page_movie, name='page_movie'),
+    path('banners/', banners, name='banners'),
+    path('cinemas/', cinemas, name='cinemas'),
+    path('news/', news, name='news'),
+    path('promotions/', promotions, name='promotions'),
+    path('pages/', pages, name='pages'),
+    path('users/', UserListView.as_view(), name='users'),
+    path('mailing/', mailing, name='mailing')
+
+
+
+
+
+
 ]
--- cinema/cms/views.py
+++ cinema/cms/views.py
@@ -1,7 +1,54 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from user.models import User
+
 
 # Create your views here.
 
+class UserListView(ListView):
+    model = User
+    context_object_name = 'user'
+    template_name = 'cms/pages/users/list_users.html'
+
 
 def index(request):
-    return render(request, 'cms/pages/index.html')
+    return render(request, 'cms/elements/base.html')
+
+
+def list_movie(request):
+    return render(request, 'cms/pages/movies/list_movie.html')
+
+
+def page_movie(request):
+    return render(request, 'cms/pages/movies/page_movie.html')
+
+
+def statistics(request):
+    return render(request, 'cms/pages/statistics.html')
+
+
+def banners(request):
+    return render(request, 'cms/pages/banners.html')
+
+
+def cinemas(request):
+    return render(request, 'cms/pages/cinemas/list_cinemas.html')
+
+
+def news(request):
+    return render(request, 'cms/pages/news/list_news.html')
+
+
+def promotions(request):
+    return render(request, 'cms/pages/promotions/list_promotions.html')
+
+
+def pages(request):
+    return render(request, 'cms/pages/page/list_pages.html')
+
+
+def mailing(request):
+    return render(request, 'cms/pages/mailing/mailing.html')
+
+
